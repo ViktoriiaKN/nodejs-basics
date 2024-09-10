3f588682140fac7308cf37463b411594f0166bcb
ViktoriiaKN <viktoriya_nykyforova@i.ua>
2024-09-10 17:46:05 +0200

    express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-const message = "Hello world";
-console.log(message);
+import path from 'node:path';
+
+// приклад для побудови шляху з його частин
+const somePath = path.join('some_folder', 'some_file.txt');
+// somePath буде 'some_folder/some_file.txt' на MacOs
+
+// somePath буде 'some_folder\\some_file.txt' на Windows
+console.log(somePath);
