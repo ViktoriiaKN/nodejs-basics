35527443b8222635fec30f88126995b43c4ab63f
ViktoriiaKN <viktoriya_nykyforova@i.ua>
2024-09-10 18:20:36 +0200

    middleware all stages

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ const app = express();
 
 const PORT = 3000;
 
+// Middleware для логування часу запиту
 app.use((req, res, next) => {
   console.log(`Time: ${new Date().toLocaleString()}`);
   next();
+});
+
+// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+// наприклад, у запитах POST або PATCH
+app.use(express.json());
+
+// Маршрут для обробки GET-запитів на '/'
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, World!',
+  });
+});
+
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
 });
 
 app.listen(PORT, () => {
