13b3dd3505f77b34089d335e91f81e4396addf81
ViktoriiaKN <viktoriya_nykyforova@i.ua>
2024-09-10 18:01:42 +0200

    get request

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
