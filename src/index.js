c77a731748e2ad96123ef9a7be0d2fc3a4f673ac
ViktoriiaKN <viktoriya_nykyforova@i.ua>
2024-09-10 18:34:24 +0200

    pino

--- src/index.js
+++ src/index.js
@@ -34,6 +34,13 @@ app.use('*', (req, res, next) => {
   });
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
